c42b6ef6293daf6166a9d782ecb6084b4cce2fe6
fede183 <fede.183@hotmail.com>
2017-11-11 19:08:20 -0300

    Validación para Todos

--- pysyslog.py
+++ pysyslog.py
@@ -9,6 +9,7 @@ import SocketServer
 import hashlib
 import hmac
 import getpass
+from Encrypter import Encrypter
 
 
 logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
@@ -30,25 +31,6 @@ class SyslogUDPHandler(SocketServer.BaseRequestHandler):
 		logging.info(newLogLine)
 
 
-
-class Encrypter():
-
-	def __init__(self, secretZero):
-		# el secreto inicial se usa lo antes posible para reducir la posibilidad de lecturas en memoria 
-		self.actualHashedKey = self.__hashForMac(secretZero)
-
-	def givemeMac(self, msg):
-		hmacGenerator = hmac.new(self.actualHashedKey, msg, hashlib.sha1)
-		self.actualHashedKey = self.__hashForMac(self.actualHashedKey)
-
-		return hmacGenerator.hexdigest()
-
-
-	def __hashForMac(self, secret):
-		# 0 para distinguir key de hash o de encripcion
-		return hashlib.sha1("0" + secret).hexdigest()
-
-
 if __name__ == "__main__":
 	try:
 		secretZero = getpass.getpass("Ingrese el secreto inicial \n")
@@ -62,9 +44,3 @@ if __name__ == "__main__":
 		raise
 	except KeyboardInterrupt:
 		print ("Crtl+C Pressed. Shutting down.")
-
-
-
-
-
-
